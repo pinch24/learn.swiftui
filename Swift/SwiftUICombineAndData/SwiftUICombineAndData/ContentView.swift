bd8272adae44f7d84cdcc2f3892ece5353366e23
mk <pinch24@gmail.com>
2022-04-04 09:24:39 +0900

    Combine - 01. Intro to SwiftUI Combine and Data

--- Swift/SwiftUICombineAndData/SwiftUICombineAndData/ContentView.swift
+++ Swift/SwiftUICombineAndData/SwiftUICombineAndData/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+		NavigationView {
+			ZStack(alignment: .top) {
+				Text("Hello, world!")
+					.padding()
+			}
+			.frame(maxHeight: .infinity, alignment: .top)
+			.background(AccountBackground())
+			.navigationBarHidden(true)
+		}
+		.navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
