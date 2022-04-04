8106cb389e9d5f3330cb88f684701bb6243fcd4c
mk <pinch24@gmail.com>
2022-04-04 09:12:17 +0900

    Combine - 01. Intro to SwiftUI Combine and Data

---
+++ Swift/SwiftUICombineAndData/SwiftUICombineAndData/SwiftUICombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineAndDataApp.swift
+//  SwiftUICombineAndData
+//
+//  Created by mk on 2022/04/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
