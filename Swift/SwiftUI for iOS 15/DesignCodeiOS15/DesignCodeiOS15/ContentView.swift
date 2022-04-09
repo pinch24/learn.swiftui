7f0b5b736f3c0107b9b101268ca658ed7f29f729
mk <pinch24@gmail.com>
2022-04-10 08:24:03 +0900

    SwiftUI - 06. Reusable Style and Color Scheme

--- Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/ContentView.swift
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/ContentView.swift
@@ -19,10 +19,11 @@ struct ContentView: View {
 				.resizable(resizingMode: .stretch)
 				.aspectRatio(contentMode: .fit)
 				.frame(width: 26.0, height: 26.0)
-				.cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+				.cornerRadius(1)
 				.padding(9)
 				.background(.ultraThinMaterial,
-							in: RoundedRectangle(cornerRadius: 20, style: .continuous))
+							in: RoundedRectangle(cornerRadius: 16, style: .continuous))
+				.strokeStyle(cornerRadius: 16)
 				
 			Text("SwiftUI for iOS 15")
 				.font(.largeTitle)
@@ -32,6 +33,7 @@ struct ContentView: View {
 						colors: [.primary, .primary.opacity(0.5)],
 						startPoint: .topLeading,
 						endPoint: .bottomTrailing))
+				.lineLimit(1)
 			
 			Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/.uppercased())
 				.font(.footnote)
@@ -53,11 +55,7 @@ struct ContentView: View {
 		//.cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
 		//.mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
 		.shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
-		.overlay(
-			RoundedRectangle(cornerRadius: 30, style: .continuous)
-				.stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
-				.blendMode(.overlay)
-		)
+		.strokeStyle()
 		.padding(.horizontal, 20)
 		.background(
 			Image("Blob 1")
