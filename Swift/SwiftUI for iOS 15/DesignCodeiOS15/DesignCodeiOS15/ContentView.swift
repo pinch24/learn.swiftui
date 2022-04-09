9b01c8f257a47cdcb8381ec7ea89503995490aee
mk <pinch24@gmail.com>
2022-04-09 09:37:28 +0900

    SwiftUI - 04. Material and Foreground Style

--- Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/ContentView.swift
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/ContentView.swift
@@ -24,6 +24,11 @@ struct ContentView: View {
 			Text("SwiftUI for iOS 15")
 				.font(.largeTitle)
 				.fontWeight(.bold)
+				.foregroundStyle(
+					.linearGradient(
+						colors: [.primary, .primary.opacity(0.5)],
+						startPoint: .topLeading,
+						endPoint: .bottomTrailing))
 			
 			Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/.uppercased())
 				.font(.footnote)
@@ -40,10 +45,19 @@ struct ContentView: View {
 		.padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
 		.padding(.vertical, 20)
 		.frame(height: 350.0)
-		.background(Color("Background"))
+		.background(.ultraThinMaterial)
 		.cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
 		.shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
 		.padding(.horizontal, 20)
+		.background(
+			Image("Blob 1")
+				.offset(x: 250, y: -100))
+		.overlay(
+			Image("Illustration 5")
+				.resizable()
+				.aspectRatio(contentMode: .fit)
+				.frame(height: 230)
+				.offset(x: 32, y: -80))
     }
 }
 
@@ -53,7 +67,6 @@ struct ContentView_Previews: PreviewProvider {
 			ContentView()
 			ContentView()
 				.preferredColorScheme(.dark)
-				.environment(\.sizeCategory, .accessibilityLarge)
 				.previewDevice("iPhone 13 mini")
 				.previewInterfaceOrientation(.portrait)
 		}
