856cbe62eb845a20e76519c4ef9867f18e8b01af
mk <pinch24@gmail.com>
2022-04-09 19:36:59 +0900

    SwiftUI - 05. Shape, Stroke and Blend Mode

--- Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/ContentView.swift
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/ContentView.swift
@@ -20,6 +20,9 @@ struct ContentView: View {
 				.aspectRatio(contentMode: .fit)
 				.frame(width: 26.0, height: 26.0)
 				.cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+				.padding(9)
+				.background(.ultraThinMaterial,
+							in: RoundedRectangle(cornerRadius: 20, style: .continuous))
 				
 			Text("SwiftUI for iOS 15")
 				.font(.largeTitle)
@@ -45,9 +48,16 @@ struct ContentView: View {
 		.padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
 		.padding(.vertical, 20)
 		.frame(height: 350.0)
-		.background(.ultraThinMaterial)
-		.cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+		.background(.ultraThinMaterial,
+					in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+		//.cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+		//.mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
 		.shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+		.overlay(
+			RoundedRectangle(cornerRadius: 30, style: .continuous)
+				.stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+				.blendMode(.overlay)
+		)
 		.padding(.horizontal, 20)
 		.background(
 			Image("Blob 1")
