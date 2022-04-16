fd67fe93b09d14fd5ee18c38cb979603d5d58cfc
mk <pinch24@gmail.com>
2022-04-16 10:59:07 +0900

    SwiftUI - 13. SVG to Path in Canvas

---
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/BlobView.swift
@@ -0,0 +1,20 @@
+//
+//  BlobView.swift
+//  DesignCodeiOS15
+//
+//  Created by mk on 2022/04/16.
+//
+
+import SwiftUI
+
+struct BlobView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BlobView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlobView()
+    }
+}
