2c696c87412775e1878e16a28a4a6510a49559bc
mk <pinch24@gmail.com>
2022-04-17 11:24:51 +0900

    SwiftUI - 22. Build a Custom Nav Bar

---
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/Components/FeaturedItem.swift
@@ -0,0 +1,76 @@
+//
+//  FeaturedItem.swift
+//  DesignCodeiOS15
+//
+//  Created by mk on 2022/04/17.
+//
+
+import SwiftUI
+
+struct FeaturedItem: View {
+	
+    var body: some View {
+		
+		VStack(alignment: .leading, spacing: 8.0) {
+			
+			Spacer()
+			
+			Image("Logo 2")
+				.resizable(resizingMode: .stretch)
+				.aspectRatio(contentMode: .fit)
+				.frame(width: 26.0, height: 26.0)
+				.cornerRadius(1)
+				.padding(9)
+				.background(.ultraThinMaterial,
+							in: RoundedRectangle(cornerRadius: 16, style: .continuous))
+				.strokeStyle(cornerRadius: 16)
+				
+			Text("SwiftUI for iOS 15")
+				.font(.largeTitle)
+				.fontWeight(.bold)
+				.foregroundStyle(
+					.linearGradient(
+						colors: [.primary, .primary.opacity(0.5)],
+						startPoint: .topLeading,
+						endPoint: .bottomTrailing))
+				.lineLimit(1)
+			
+			Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/.uppercased())
+				.font(.footnote)
+				.fontWeight(.semibold)
+				.foregroundColor(.secondary)
+			
+			Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+				.font(.footnote)
+				.multilineTextAlignment(.leading)
+				.lineLimit(2)
+				.frame(maxWidth: .infinity, alignment: .leading)
+				.foregroundColor(.secondary)
+		}
+		.padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+		.padding(.vertical, 20)
+		.frame(height: 350.0)
+		.background(.ultraThinMaterial,
+					in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+		//.cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+		//.mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+		.shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+		.strokeStyle()
+		.padding(.horizontal, 20)
+		.background(
+			Image("Blob 1")
+				.offset(x: 250, y: -100))
+		.overlay(
+			Image("Illustration 5")
+				.resizable()
+				.aspectRatio(contentMode: .fit)
+				.frame(height: 230)
+			.offset(x: 32, y: -80))
+    }
+}
+
+struct FeaturedItem_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedItem()
+    }
+}
