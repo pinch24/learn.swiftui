63a70a9a81c84d10c20d95cfe5c84a118183df29
mk <pinch24@gmail.com>
2022-05-01 12:46:01 +0900

    SwiftUI - 44. Matched Geometry with Data

--- Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/Views/HomeView.swift
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/Views/HomeView.swift
@@ -34,13 +34,15 @@ struct HomeView: View {
 					.padding(.horizontal, 20)
 				
 				if !show {
-					CourseItem(namespace: namespace, show: $show)
-						.onTapGesture {
-							withAnimation(.openCard) {
-								show.toggle()
-								showStatusBar = false
-							}
+					ForEach(courses) { course in
+						CourseItem(namespace: namespace, course: course, show: $show)
+							.onTapGesture {
+								withAnimation(.openCard) {
+									show.toggle()
+									showStatusBar = false
+								}
 						}
+					}
 				}
 			}
 			.coordinateSpace(name: "scroll")
@@ -51,12 +53,14 @@ struct HomeView: View {
 				NavigationBar(hasScrolled: $hasScrolled, title: "Featured"))
 			
 			if show {
-				CourseView(namespace: namespace, show: $show)
-					.zIndex(1)
-					.transition(
-						.asymmetric(
-							insertion: .opacity.animation(.easeInOut(duration: 0.1)),
+				ForEach(courses) { course in
+					CourseView(namespace: namespace, course: course, show: $show)
+						.zIndex(1)
+						.transition(
+							.asymmetric(
+								insertion: .opacity.animation(.easeInOut(duration: 0.1)),
 							removal: .opacity.animation(.easeInOut(duration: 0.3).delay(0.2))))
+				}
 			}
 		}
 		 .statusBar(hidden: !showStatusBar)
@@ -92,7 +96,7 @@ struct HomeView: View {
 	var featured: some View {
 		
 		TabView {
-			ForEach(courses) { course in
+			ForEach(featuredCourses) { course in
 				GeometryReader { proxy in
 					
 					let minX = proxy.frame(in: .global).minX
