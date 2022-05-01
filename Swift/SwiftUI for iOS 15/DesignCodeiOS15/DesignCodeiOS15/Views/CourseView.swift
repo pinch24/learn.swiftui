78f5edd289e217192cd8fe29cf85d8addba7a1fd
mk <pinch24@gmail.com>
2022-05-01 17:59:46 +0900

    SwiftUI - 49. Edge Pan Gesture

--- Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/Views/CourseView.swift
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/Views/CourseView.swift
@@ -14,6 +14,7 @@ struct CourseView: View {
 	@Binding var show: Bool
 	@State var appear = [false, false, false]
 	@State var viewState = CGSize.zero
+	@State var isDraggable = true
 	@EnvironmentObject var model: Model
 	
     var body: some View {
@@ -35,17 +36,7 @@ struct CourseView: View {
 			.scaleEffect(viewState.width / -500 + 1)
 			.background(.black.opacity(viewState.width / 500))
 			.background(.ultraThinMaterial)
-			.gesture(
-				DragGesture()
-					.onChanged { value in
-						guard value.translation.width > 0 else { return }
-						viewState = value.translation
-					}
-					.onEnded { value in
-						withAnimation(.closeCard) {
-							viewState = .zero
-						}
-					})
+			.gesture(isDraggable ? drag : nil)
 			.ignoresSafeArea()
 			
 			button
@@ -186,6 +177,33 @@ struct CourseView: View {
 			.padding(20)
 	}
 	
+	var drag: some Gesture {
+		
+		DragGesture(minimumDistance: 30, coordinateSpace: .local)
+			.onChanged { value in
+				guard value.translation.width > 0 else { return }
+				if value.startLocation.x < 100 {
+					withAnimation(.closeCard) {
+						viewState = value.translation
+					}
+				}
+				
+				if viewState.width > 120 {
+					close()
+				}
+			}
+			.onEnded { value in
+				if viewState.width > 80 {
+					close()
+				}
+				else {
+					withAnimation(.closeCard) {
+						viewState = .zero
+					}
+				}
+			}
+	}
+	
 	func fadeIn() {
 		withAnimation(.easeOut.delay(0.3)) { appear[0] = true }
 		withAnimation(.easeOut.delay(0.4)) { appear[1] = true }
@@ -196,6 +214,20 @@ struct CourseView: View {
 		appear[0] = false
 		appear[1] = false
 		appear[2] = false
+	}
+	
+	func close() {
+		
+		withAnimation(.closeCard.delay(0.3)) {
+			show.toggle()
+			model.showDetail.toggle()
+		}
+		
+		withAnimation(.closeCard) {
+			viewState = .zero
+		}
+		
+		isDraggable = false
 	}
 }
 
