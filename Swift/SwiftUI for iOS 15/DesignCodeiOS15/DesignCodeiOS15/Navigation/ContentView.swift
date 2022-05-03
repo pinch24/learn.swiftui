05325d0c12a70f866a027e91930f1b314d385a88
mk <pinch24@gmail.com>
2022-05-04 07:54:27 +0900

    SwiftUI - 54. Custom Modal and Button Style

--- Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/Navigation/ContentView.swift
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/Navigation/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
 	
 	@AppStorage("selectedTab") var selectedTab: Tab = .home
+	@AppStorage("showModal") var showModal = false
 	@EnvironmentObject var model: Model
 	
     var body: some View {
@@ -29,6 +30,32 @@ struct ContentView: View {
 			
 			TabBar()
 				.offset(y: model.showDetail ? 200 : 0)
+			
+			if showModal {
+				ZStack {
+					
+					Color.clear.background(.regularMaterial)
+						.ignoresSafeArea()
+					
+					SignUpView()
+					
+					Button {
+						withAnimation {
+							showModal = false
+						}
+					} label: {
+						
+						Image(systemName: "xmark")
+							.font(.body.weight(.bold))
+							.foregroundColor(.secondary)
+							.padding(8)
+							.background(.ultraThinMaterial, in: Circle())
+					}
+					.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+					.padding(20)
+				}
+				.zIndex(1)
+			}
 		}
 		.safeAreaInset(edge: .bottom, spacing: 0) {
 			Color.clear.frame(height: 44)
@@ -40,6 +67,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
 		Group {
 			ContentView()
+				.preferredColorScheme(.dark)
 			ContentView()
 				.preferredColorScheme(.dark)
 				.previewDevice("iPhone 13 mini")
