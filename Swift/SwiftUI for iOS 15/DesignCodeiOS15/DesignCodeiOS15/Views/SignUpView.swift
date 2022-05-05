2d8197b16cb4879122f39db6368e78745d2317ba
mk <pinch24@gmail.com>
2022-05-05 16:38:41 +0900

    SwiftUI - 58. Animated Focus States

--- Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/Views/SignUpView.swift
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/Views/SignUpView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct SignUpView: View {
 	
+	enum Field: Hashable {
+		case email
+		case password
+	}
+	
 	@State var email = ""
 	@State var password = ""
+	@FocusState var focusedField: Field?
+	@State var circleY: CGFloat = 120
 	
     var body: some View {
 		
@@ -28,10 +35,14 @@ struct SignUpView: View {
 				.keyboardType(.emailAddress)
 				.autocapitalization(.none)
 				.disableAutocorrection(true)
+				.focused($focusedField, equals: .email)
+				.shadow(color: focusedField == .email ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
 			
 			SecureField("Password", text: $password)
 				.inputStyle(icon: "lock")
 				.textContentType(.password)
+				.focused($focusedField, equals: .password)
+				.shadow(color: focusedField == .password ? .primary.opacity(0.3) : .clear, radius: 10, x: 0, y: 3)
 				
 			Button {
 				
@@ -70,10 +81,27 @@ struct SignUpView: View {
 		}
 		.padding(20)
 		.background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+		.background(
+			Circle()
+				.fill(.blue)
+				.frame(width: 68, height: 68)
+				.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+				.offset(y: circleY)
+		)
 		.strokeStyle(cornerRadius: 30)
 		.shadow(color: Color("Shadow").opacity(0.2), radius: 30, x: 0, y: 30)
 		.padding(20)
 		.background(Image("Blob 1").offset(x: 200, y: -100))
+		.onChange(of: focusedField) { value in
+			withAnimation {
+				if value == .email {
+					circleY = 120
+				}
+				else {
+					circleY = 190
+				}
+			}
+		}
     }
 }
 
@@ -81,7 +109,9 @@ struct SignUpView_Previews: PreviewProvider {
 	
     static var previews: some View {
 		
-		SignUpView()
-			.preferredColorScheme(.light)
+		ZStack {
+			SignUpView()
+				.preferredColorScheme(.light)
+		}
     }
 }
