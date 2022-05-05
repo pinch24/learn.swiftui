3e44f5876e4ec56fc8f898e68e6c1b900f5fcf29
mk <pinch24@gmail.com>
2022-05-05 19:13:51 +0900

    SwiftUI - 63. AsyncImage

--- Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/Navigation/NavigationBar.swift
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15/DesignCodeiOS15/Navigation/NavigationBar.swift
@@ -52,13 +52,7 @@ struct NavigationBar: View {
 //					showAccount = true
 					showModal = true
 				} label: {
-					Image("Avatar Default")
-						.resizable()
-						.frame(width: 26, height: 26)
-						.cornerRadius(10)
-						.padding(8)
-						.background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 18, style: .continuous))
-						.strokeStyle(cornerRadius: 18)
+					AvatarView()
 				}
 				.sheet(isPresented: $showAccount) {
 					AccountView()
