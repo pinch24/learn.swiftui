195149eb22789459864581b568b292548154129f
mk <pinch24@gmail.com>
2022-05-15 16:33:18 +0900

    Combine - 03. NavigationLink and Link

--- Swift/SwiftUICombineAndData/SwiftUICombineAndData/ContentView.swift
+++ Swift/SwiftUICombineAndData/SwiftUICombineAndData/ContentView.swift
@@ -18,13 +18,9 @@ struct ContentView: View {
 			ZStack(alignment: .top) {
 				
 				TrackableScrollView(offsetChanged: { offset in
-					
 					contentOffset = offset.y
-					print("contentOffset", contentOffset)
 				}) {
-					
-					Text("Hello, world!")
-						.padding()
+					content
 				}
 				
 				VisualEffectBlur(blurStyle: .systemMaterial)
@@ -40,6 +36,54 @@ struct ContentView: View {
 		}
 		.navigationViewStyle(StackNavigationViewStyle())
     }
+	
+	var content: some View {
+		
+		VStack {
+			
+			// More Content
+			VStack {
+				
+				NavigationLink(destination: FAQView()) {
+					MenuRow()
+				}
+				
+				divider
+				
+				NavigationLink(destination: PackagesView()) {
+					MenuRow(title: "SwiftUI Packages", leftIcon: "square.stack.3d.up.fill")
+				}
+				
+				divider
+				
+				Link(destination: URL(string: "https://www.youtube.com/channel/UCTIhfOopxukTIRkbXJ3kN-g")!) {
+					MenuRow(title: "YouTube Channel", leftIcon: "play.rectangle.fill", rightIcon: "link")
+				}
+			}
+			.padding(16)
+			.background(Color("Background 1"))
+			.background(VisualEffectBlur(blurStyle: .systemUltraThinMaterialDark))
+			.overlay(RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(Color.white, lineWidth: 1).blendMode(.overlay))
+			.mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
+			.padding(.top, 20)
+			
+			Text("Version 1.00")
+				.foregroundColor(.white.opacity(0.7))
+				.padding(.top, 20)
+				.padding(.horizontal, 20)
+				.padding(.bottom, 10)
+				.font(.footnote)
+		}
+		.foregroundColor(.white)
+		.padding(.top, 20)
+		.padding(.horizontal, 20)
+		.padding(.bottom, 10)
+	}
+	
+	var divider: some View {
+		
+		Divider().background(Color.white.blendMode(.overlay))
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Swift/SwiftUICombineAndData/SwiftUICombineAndData/Views/FAQView.swift
@@ -0,0 +1,20 @@
+//
+//  FAQView.swift
+//  SwiftUICombineAndData
+//
+//  Created by mk on 2022/05/15.
+//
+
+import SwiftUI
+
+struct FAQView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FAQView_Previews: PreviewProvider {
+    static var previews: some View {
+        FAQView()
+    }
+}
