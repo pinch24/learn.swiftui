d4aac01105de31244c51d5e78b257cfd9c85e2e2
mk <pinch24@gmail.com>
2022-05-31 16:58:56 +0900

    SwiftUI - 01. Build a SwiftUI App for iOS 15

---
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15a/DesignCodeiOS15a/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodeiOS15a
+//
+//  Created by mk on 2022/05/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15a/DesignCodeiOS15a/DesignCodeiOS15aApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15aApp.swift
+//  DesignCodeiOS15a
+//
+//  Created by mk on 2022/05/31.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15aApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
