a1cf5e53bd389dd948df67f20cad9e44398c15b1
mk <pinch24@gmail.com>
2022-07-12 12:52:58 +0900

    SwiftUI - 19. Tab Bar Animation

--- Swift/SwiftUI for iOS 15/DesignCodeiOS15a/DesignCodeiOS15a/TabBar.swift
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15a/DesignCodeiOS15a/TabBar.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct TabBar: View {
 	
 	@State var selectedTab: Tab = .home
+	@State var color: Color = .teal
 	
     var body: some View {
 		
@@ -35,7 +36,10 @@ struct TabBar: View {
 				ForEach(tabItems) { item in
 					
 					Button {
-						selectedTab = item.tab
+						withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+							selectedTab = item.tab
+							color = item.color
+						}
 					} label: {
 						VStack(spacing: 0) {
 							Image(systemName: item.icon)
@@ -49,6 +53,7 @@ struct TabBar: View {
 						.frame(maxWidth: .infinity)
 					}
 					.foregroundColor(selectedTab == item.tab ? .primary : .secondary)
+					.blendMode(selectedTab == item.tab ? .overlay : .normal)
 					
 					Spacer()
 				}
@@ -57,6 +62,35 @@ struct TabBar: View {
 			.padding(.top, 14)
 			.frame(height: 88, alignment: .top)
 			.background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
+			.background(
+				HStack {
+					if selectedTab == .library { Spacer() }
+					if selectedTab == .explore { Spacer() }
+					if selectedTab == .notifications { Spacer(); Spacer() }
+					Circle().fill(color).frame(width: 88)
+					if selectedTab == .home { Spacer() }
+					if selectedTab == .explore { Spacer(); Spacer() }
+					if selectedTab == .notifications { Spacer() }
+				}
+				.padding(.horizontal, 8)
+			)
+			.overlay(
+				HStack {
+					if selectedTab == .library { Spacer() }
+					if selectedTab == .explore { Spacer() }
+					if selectedTab == .notifications { Spacer(); Spacer() }
+					Rectangle()
+						.fill(color)
+						.frame(width: 28, height: 5)
+						.cornerRadius(3)
+						.frame(width: 88)
+						.frame(maxHeight: .infinity, alignment: .top)
+					if selectedTab == .home { Spacer() }
+					if selectedTab == .explore { Spacer(); Spacer() }
+					if selectedTab == .notifications { Spacer() }
+				}
+				//.padding(.horizontal, 8)
+			)
 			.strokeStyle(cornerRadius: 34)
 			.frame(maxHeight: .infinity, alignment: .bottom)
 			.ignoresSafeArea()
