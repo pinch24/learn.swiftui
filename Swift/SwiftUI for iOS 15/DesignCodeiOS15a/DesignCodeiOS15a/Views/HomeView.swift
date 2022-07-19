8a37d4ea7b9b025e82f3a3e72e1aca13c7b70697
mk <pinch24@gmail.com>
2022-07-19 10:01:11 +0900

    SwiftUI - 27. Tab View Pagination

--- Swift/SwiftUI for iOS 15/DesignCodeiOS15a/DesignCodeiOS15a/Views/HomeView.swift
+++ Swift/SwiftUI for iOS 15/DesignCodeiOS15a/DesignCodeiOS15a/Views/HomeView.swift
@@ -12,32 +12,53 @@ struct HomeView: View {
 	@State var hasScrolled = false
 	
     var body: some View {
-		
-		ScrollView {
+		ZStack {
+			Color("Background")
+				.ignoresSafeArea()
 			
-			GeometryReader { proxy in
-				Text("\(proxy.frame(in: .named("scroll")).minY)")
-				Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+			ScrollView {
+				scrollDetection
+				
+				featured
+				
+				Color.clear.frame(height: 1000)
 			}
-			.frame(height: 0)
-			
-			FeaturedItem()
-			
-			Color.clear.frame(height: 1000)
+			.coordinateSpace(name: "scroll")
+			.onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
+				withAnimation(.easeInOut) {
+					hasScrolled = value < 0 ? true : false
+				}
+			})
+			.safeAreaInset(edge: .top, content: {
+				Color.clear.frame(height: 70)
+			})
+			.overlay(
+				NavigationBar(hasScrolled: $hasScrolled, title: "Featured")
+			)
 		}
-		.coordinateSpace(name: "scroll")
-		.onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
-			withAnimation(.easeInOut) {
-				hasScrolled = value < 0 ? true : false
-			}
-		})
-		.safeAreaInset(edge: .top, content: {
-			Color.clear.frame(height: 70)
-		})
-		.overlay(
-			NavigationBar(hasScrolled: $hasScrolled, title: "Featured")
-		)
     }
+	
+	var scrollDetection: some View {
+		GeometryReader { proxy in
+			//Text("\(proxy.frame(in: .named("scroll")).minY)")
+			Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+		}
+		.frame(height: 0)
+	}
+	
+	var featured: some View {
+		TabView {
+			ForEach(courses) { item in
+				FeaturedItem(course: item)
+			}
+		}
+		.tabViewStyle(.page(indexDisplayMode: .never))
+		.frame(height: 430)
+		.background(
+			Image("Blob 1")
+				.offset(x: 250, y: -100)
+		)
+	}
 }
 
 struct HomeView_Previews: PreviewProvider {
