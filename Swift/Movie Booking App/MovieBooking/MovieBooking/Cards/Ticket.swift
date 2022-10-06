4f33f190b07db0b3f7bcc6910ad0ebd398a423f3
mk <pinch24@gmail.com>
2022-10-06 13:47:45 +0900

    03. Ticket Stack

--- Swift/Movie Booking App/MovieBooking/MovieBooking/Cards/Ticket.swift
+++ Swift/Movie Booking App/MovieBooking/MovieBooking/Cards/Ticket.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct Ticket: View {
+	@Binding var height: CGFloat
+	
 	@State var title = "Thor"
 	@State var subtitle = "Love and Thunder"
 	@State var top = "thor-top"
+	@State var bottom = "thor-bottom"
 	
 	var gradient = [Color("cyan"), Color("cyan").opacity(0), Color("cyan").opacity(0)]
 	
@@ -40,6 +43,63 @@ struct Ticket: View {
 					.stroke(LinearGradient(colors: gradient, startPoint: .topLeading, endPoint: .bottomTrailing), style: StrokeStyle(lineWidth: 2))
 			)
 			.cornerRadius(40, corners: [.topLeft, .topRight])
+			
+			Spacer(minLength: height)
+			
+			VStack(spacing: 10) {
+				Line()
+					.stroke(style: StrokeStyle(lineWidth: 2, dash: [7]))
+					.frame(width: 200, height: 1)
+					.opacity(0.6)
+				
+				HStack(spacing: 20) {
+					HStack(spacing: 4) {
+						Text("Date:")
+							.fontWeight(.medium)
+							.foregroundColor(Color("lightPurple"))
+						Text("April 23")
+							.foregroundColor(.black)
+					}
+					HStack(spacing: 4) {
+						Text("Time:")
+							.fontWeight(.medium)
+							.foregroundColor(Color("lightPurple"))
+						Text("6 p.m.")
+							.foregroundColor(.black)
+					}
+				}
+				
+				HStack(spacing: 20) {
+					HStack(spacing: 4) {
+						Text("Row:")
+							.fontWeight(.medium)
+							.foregroundColor(Color("lightPurple"))
+						Text("2")
+							.foregroundColor(.black)
+					}
+					HStack(spacing: 4) {
+						Text("Seats:")
+							.fontWeight(.medium)
+							.foregroundColor(Color("lightPurple"))
+						Text("9, 10")
+							.foregroundColor(.black)
+					}
+				}
+				
+				Image("code")
+			}
+			.frame(width: 250, height: 135, alignment: .top)
+			.background(.ultraThinMaterial)
+			.background(
+				Image(bottom)
+					.resizable()
+					.aspectRatio(contentMode: .fill)
+			)
+			.mask(
+				Image(bottom)
+					.resizable()
+					.aspectRatio(contentMode: .fill)
+			)
 		}
 		.frame(height: 460)
 		.font(.footnote)
@@ -49,6 +109,6 @@ struct Ticket: View {
 
 struct Ticket_Previews: PreviewProvider {
     static var previews: some View {
-        Ticket()
+		Ticket(height: .constant(0))
     }
 }
