047c9e07e09d7951ec989f9406039e02096174b4
mk <pinch24@gmail.com>
2022-10-06 10:56:33 +0900

    01. Intro

---
+++ Swift/Movie Booking App/MovieBooking/MovieBooking/MovieBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingApp.swift
+//  MovieBooking
+//
+//  Created by mk on 2022/10/06.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
