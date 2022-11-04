82e293a2ca3ee989bbdbdf5bb0cf86272b36bf0a
mk <pinch24@gmail.com>
2022-11-04 10:49:57 +0900

    11. Seats View

--- Swift/Movie Booking App/MovieBooking/MovieBooking/Views/SeatsView.swift
+++ Swift/Movie Booking App/MovieBooking/MovieBooking/Views/SeatsView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct SeatsView: View {
 	@Environment(\.dismiss) var dismiss
 	
+	@State var animated = false
+	@State var showButton = false
+	
     var body: some View {
 		VStack(spacing: 0) {
 			HStack {
@@ -39,6 +42,11 @@ struct SeatsView: View {
 				.frame(height: 240)
 				.padding(.top, 60)
 				.padding(.horizontal, 20)
+				.onTapGesture {
+					withAnimation(.spring()) {
+						showButton = true
+					}
+				}
 			
 			HStack(spacing: 2) {
 				StatusUI()
@@ -46,6 +54,62 @@ struct SeatsView: View {
 				StatusUI(color: Color("cyan"), text: "Selected")
 			}
 			.padding(.top, 60)
+			
+			ZStack(alignment: .topLeading) {
+				Circle()
+					.frame(width: 200, height: 200)
+					.foregroundColor(Color("purple"))
+					.blur(radius: animated ? 70 : 100)
+					.offset(x: animated ? -100 : 20, y: animated ? -20 : 20)
+					.task {
+						withAnimation(.easeInOut(duration: 7).repeatForever()) {
+							animated.toggle()
+						}
+					}
+				
+				Circle()
+					.frame(width: 200, height: 200)
+					.foregroundColor(Color("lightBlue"))
+					.blur(radius: animated ? 50 : 100)
+					.offset(x: animated ? 50 : 70, y: animated ? 70 : 30)
+				
+				Circle()
+					.frame(width: 200, height: 200)
+					.foregroundColor(Color("pink"))
+					.blur(radius: animated ? 50 : 100)
+					.offset(x: animated ? 150 : 170, y: animated ? 90 : 100)
+				
+				VStack(alignment: .leading, spacing: 30) {
+					HStack(spacing: 10) {
+						Image(systemName: "calendar")
+						Text("November 4, 2022")
+						Circle()
+							.frame(width: 6, height: 6)
+						Text("6 p.m.")
+					}
+					
+					HStack(spacing: 10) {
+						Image(systemName: "ticket.fill")
+						Text("VIP Section")
+						Circle()
+							.frame(width: 6, height: 6)
+						Text("Seat 9, 10")
+					}
+					
+					HStack(spacing: 10) {
+						Image(systemName: "cart.fill")
+						Text("Total: $30")
+					}
+				}
+				.padding(42)
+				.font(.subheadline)
+			}
+			.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+			.clipped()
+			.foregroundColor(.white)
+			.background(.ultraThinMaterial)
+			.padding(.top, 50)
+			.offset(y: showButton ? 0 : 250)
 		}
 		.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
 		.background(Color("backgroundColor"))
