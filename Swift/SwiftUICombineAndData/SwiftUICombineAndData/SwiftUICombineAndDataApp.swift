eb6eac34fb9dc00e8e902bf17fc4f973d6e5df0a
mk <pinch24@gmail.com>
2022-12-29 18:57:32 +0900

    SwiftUI Combine and Data - 14. Configuring Notifications in AppDelegate

--- Swift/SwiftUICombineAndData/SwiftUICombineAndData/SwiftUICombineAndDataApp.swift
+++ Swift/SwiftUICombineAndData/SwiftUICombineAndData/SwiftUICombineAndDataApp.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import Firebase
+import FirebaseMessaging
+import UserNotifications
 
 @main
 struct SwiftUICombineAndDataApp: App {
@@ -19,8 +21,54 @@ struct SwiftUICombineAndDataApp: App {
 }
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+	let gcmMessageIDKey = "gcm.message_id"
+	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
 		FirebaseApp.configure()
+		
+		Messaging.messaging().delegate = self
+		UNUserNotificationCenter.current().delegate = self
+		let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+		UNUserNotificationCenter.current().requestAuthorization(options: authOptions, completionHandler: { _, _ in })
+		application.registerForRemoteNotifications()
+		
 		return true
+	}
+	
+	func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+		print("application:didRegisterForRemoteNotificationsWithDeviceToken:")
+	}
+	
+	func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+		print("application:didFailToRegisterForRemoteNotificationsWithError:")
+	}
+	
+	func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+		let messageId = userInfo[gcmMessageIDKey] {
+			print("Message ID: \(messageId)")
+		}
+		print(userInfo)
+		
+		completionHandler(UIBackgroundFetchResult.newData)
+	}
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+	func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+		let userInfo = notification.request.content.userInfo
+		print(userInfo)
+		return [[.banner, .badge, .sound]]
+	}
+	
+	func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+		let userInfo = response.notification.request.content.userInfo
+		print(userInfo)
+	}
+}
+
+extension AppDelegate: MessagingDelegate {
+	func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+		let deviceToken: [String: String] = ["token": fcmToken ?? ""]
+		print("Device Token: ", deviceToken)
 	}
 }
