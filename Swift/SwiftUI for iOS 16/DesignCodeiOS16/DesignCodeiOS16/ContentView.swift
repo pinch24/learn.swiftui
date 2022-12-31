a0fa279c83cd83cc124ccc3b395930d50e783a44
mk <pinch24@gmail.com>
2023-01-01 02:46:05 +0900

    SwiftUI iOS 16 - 02. Xcode 14 Visual Editor

--- Swift/SwiftUI for iOS 16/DesignCodeiOS16/DesignCodeiOS16/ContentView.swift
+++ Swift/SwiftUI for iOS 16/DesignCodeiOS16/DesignCodeiOS16/ContentView.swift
@@ -9,13 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+		VStack(alignment: .center, spacing: 20.0) {
+			Image(systemName: "timelapse")
+				.imageScale(.large)
+				.foregroundColor(.accentColor)
+				.font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+			Text("Switching Apps")
+				.font(.largeTitle)
+				.fontWeight(.bold)
+			Text("Tap and hold any part of the screen for 1 second to show the menu for switching between apps.")
+				.multilineTextAlignment(.center)
+			Button("Got it") {
+				
+			}
+			.padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+			.frame(width: 300.0)
+			.border(/*@START_MENU_TOKEN@*/Color("AccentColor")/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+			
+		}
+		.padding()
     }
 }
 
