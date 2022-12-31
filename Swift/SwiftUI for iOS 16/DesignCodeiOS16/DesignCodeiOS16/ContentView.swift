83d979b4a9c760aa9a87e06d5a0d611e3d09baec
mk <pinch24@gmail.com>
2023-01-01 03:07:01 +0900

    SwiftUI iOS 16 - 03. Basic Layout and SF Symbols 4

--- Swift/SwiftUI for iOS 16/DesignCodeiOS16/DesignCodeiOS16/ContentView.swift
+++ Swift/SwiftUI for iOS 16/DesignCodeiOS16/DesignCodeiOS16/ContentView.swift
@@ -10,29 +10,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
 		VStack(alignment: .center, spacing: 20.0) {
-			Image(systemName: "timelapse")
+			Image(systemName: "timelapse", variableValue: 0.2)
 				.imageScale(.large)
 				.foregroundColor(.accentColor)
-				.font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+				.font(.system(size: 50))
+				.fontWeight(.thin)
 			Text("Switching Apps")
 				.font(.largeTitle)
 				.fontWeight(.bold)
 			Text("Tap and hold any part of the screen for 1 second to show the menu for switching between apps.")
 				.multilineTextAlignment(.center)
-			Button("Got it") {
-				
+			Button {
+				// Action
+			} label: {
+				Text("Got it")
+					.padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+					.frame(maxWidth: .infinity)
+					.background(
+						RoundedRectangle(cornerRadius: 10)
+							.stroke()
+					)
 			}
-			.padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-			.frame(width: 300.0)
-			.border(/*@START_MENU_TOKEN@*/Color("AccentColor")/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-			
+			.accentColor(.primary)
 		}
-		.padding()
+		.padding(30)
+		.background(.ultraThinMaterial)
+		.cornerRadius(10)
+		.overlay(RoundedRectangle(cornerRadius: 10).stroke())
+		.padding(10)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+			.background(Image("Wallpaper 2"))
     }
 }
