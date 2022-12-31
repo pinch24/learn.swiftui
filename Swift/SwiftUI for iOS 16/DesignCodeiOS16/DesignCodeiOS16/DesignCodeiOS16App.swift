405e65e9efda637c610b405347e5b65a657dac1b
mk <pinch24@gmail.com>
2023-01-01 02:24:44 +0900

    SwiftUI iOS 16 - 01. Build SwiftUI apps for iOS 16

---
+++ Swift/SwiftUI for iOS 16/DesignCodeiOS16/DesignCodeiOS16/DesignCodeiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS16App.swift
+//  DesignCodeiOS16
+//
+//  Created by mk on 2023/01/01.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
