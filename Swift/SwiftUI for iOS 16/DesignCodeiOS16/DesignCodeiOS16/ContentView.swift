5aa8dceff8c975c8999bf3c7bb4a8dac531feab2
mk <pinch24@gmail.com>
2023-01-01 12:01:46 +0900

    SwiftUI 16 - 05. Variable Color and Animation

--- Swift/SwiftUI for iOS 16/DesignCodeiOS16/DesignCodeiOS16/ContentView.swift
+++ Swift/SwiftUI for iOS 16/DesignCodeiOS16/DesignCodeiOS16/ContentView.swift
@@ -8,13 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+	@State var time = 0.0
+	@State var showMessage = true
+	let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+	
+	var body: some View {
+		content
+			.opacity(showMessage ? 1 : 0)
+			.scaleEffect(showMessage ? 1 : 0)
+			.rotationEffect(.degrees(showMessage ? 0 : 30))
+			.offset(y: showMessage ? 0 : 500)
+			.blur(radius: showMessage ? 0 : 20)
+			.padding(10)
+			.dynamicTypeSize(.xSmall ... .xxxLarge)
+	}
+	
+    var content: some View {
 		VStack(alignment: .center, spacing: 20.0) {
-			Image(systemName: "timelapse", variableValue: 0.2)
+			Image(systemName: "timelapse", variableValue: time)
 				.imageScale(.large)
 				.foregroundColor(.accentColor)
 				.font(.system(size: 50))
 				.fontWeight(.thin)
+				.onReceive(timer) { value in
+					if time < 1.0 {
+						time += 0.1
+					}
+					else {
+						// time = 0.0
+					}
+				}
 			Text("Switching Apps".uppercased())
 				.font(.largeTitle.width(.condensed))
 				.fontWeight(.bold)
@@ -23,7 +46,9 @@ struct ContentView: View {
 				.foregroundColor(.secondary)
 				.fontWeight(.medium)
 			Button {
-				// Action
+				withAnimation(.easeInOut) {
+					showMessage = false
+				}
 			} label: {
 				Text("Got it")
 					.padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
@@ -31,9 +56,7 @@ struct ContentView: View {
 					.background(.white.opacity(0.2).gradient)
 					.cornerRadius(10)
 					.background(
-						RoundedRectangle(cornerRadius: 10)
-							.stroke()
-							.foregroundStyle(.linearGradient(colors: [.white.opacity(0.5), .clear, .white.opacity(0.5), .clear], startPoint: .topLeading, endPoint: .bottomTrailing))
+						stroke
 					)
 			}
 			.accentColor(.primary)
@@ -43,15 +66,17 @@ struct ContentView: View {
 		.background(.ultraThinMaterial)
 		.cornerRadius(10)
 		.overlay(
-			RoundedRectangle(cornerRadius: 10)
-				.stroke()
-				.foregroundStyle(.linearGradient(colors: [.white.opacity(0.5), .clear, .white.opacity(0.5), .clear], startPoint: .topLeading, endPoint: .bottomTrailing))
+			stroke
 		)
 		.shadow(color: .black.opacity(0.3), radius: 20, y: 20)
 		.frame(maxWidth: 500)
-		.padding(10)
-		.dynamicTypeSize(.xSmall ... .xxxLarge)
     }
+	
+	var stroke: some View {
+		RoundedRectangle(cornerRadius: 10)
+			.stroke()
+			.foregroundStyle(.linearGradient(colors: [.white.opacity(0.5), .clear, .white.opacity(0.5), .clear], startPoint: .topLeading, endPoint: .bottomTrailing))
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
