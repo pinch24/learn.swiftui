f3ef92b9ef51d3b662b6550c69ab1dc71e4ac32d
mk <pinch24@gmail.com>
2023-01-01 10:11:17 +0900

    SwiftUI iOS 16 - 04. SF Width Styles and Gradients

--- Swift/SwiftUI for iOS 16/DesignCodeiOS16/DesignCodeiOS16/ContentView.swift
+++ Swift/SwiftUI for iOS 16/DesignCodeiOS16/DesignCodeiOS16/ContentView.swift
@@ -15,29 +15,42 @@ struct ContentView: View {
 				.foregroundColor(.accentColor)
 				.font(.system(size: 50))
 				.fontWeight(.thin)
-			Text("Switching Apps")
-				.font(.largeTitle)
+			Text("Switching Apps".uppercased())
+				.font(.largeTitle.width(.condensed))
 				.fontWeight(.bold)
 			Text("Tap and hold any part of the screen for 1 second to show the menu for switching between apps.")
 				.multilineTextAlignment(.center)
+				.foregroundColor(.secondary)
+				.fontWeight(.medium)
 			Button {
 				// Action
 			} label: {
 				Text("Got it")
 					.padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
 					.frame(maxWidth: .infinity)
+					.background(.white.opacity(0.2).gradient)
+					.cornerRadius(10)
 					.background(
 						RoundedRectangle(cornerRadius: 10)
 							.stroke()
+							.foregroundStyle(.linearGradient(colors: [.white.opacity(0.5), .clear, .white.opacity(0.5), .clear], startPoint: .topLeading, endPoint: .bottomTrailing))
 					)
 			}
 			.accentColor(.primary)
+			.shadow(radius: 10)
 		}
 		.padding(30)
 		.background(.ultraThinMaterial)
 		.cornerRadius(10)
-		.overlay(RoundedRectangle(cornerRadius: 10).stroke())
+		.overlay(
+			RoundedRectangle(cornerRadius: 10)
+				.stroke()
+				.foregroundStyle(.linearGradient(colors: [.white.opacity(0.5), .clear, .white.opacity(0.5), .clear], startPoint: .topLeading, endPoint: .bottomTrailing))
+		)
+		.shadow(color: .black.opacity(0.3), radius: 20, y: 20)
+		.frame(maxWidth: 500)
 		.padding(10)
+		.dynamicTypeSize(.xSmall ... .xxxLarge)
     }
 }
 
