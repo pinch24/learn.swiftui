89319e43d167b3e912349ea00a83a633ee3b521d
mk <pinch24@gmail.com>
2023-01-19 17:51:08 +0900

    SwiftUI Concurrency - 01. Intro

---
+++ DesignCode/SwiftUI Concurrency/SwiftUIConcurrency/SwiftUIConcurrency/SwiftUIConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConcurrencyApp.swift
+//  SwiftUIConcurrency
+//
+//  Created by mk on 2023/01/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
