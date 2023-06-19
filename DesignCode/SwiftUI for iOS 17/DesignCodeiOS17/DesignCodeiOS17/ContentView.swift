29ce135a4e83cc1307d213d90bc94412dff43907
mk <pinch24@gmail.com>
2023-06-19 11:22:41 +0900

    SwiftUI iOS 17 - 01. Build SwiftUI Apps for IOS 17

---
+++ DesignCode/SwiftUI for iOS 17/DesignCodeiOS17/DesignCodeiOS17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignCodeiOS17
+//
+//  Created by mk on 2023/06/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignCode/SwiftUI for iOS 17/DesignCodeiOS17/DesignCodeiOS17/DesignCodeiOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS17App.swift
+//  DesignCodeiOS17
+//
+//  Created by mk on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
