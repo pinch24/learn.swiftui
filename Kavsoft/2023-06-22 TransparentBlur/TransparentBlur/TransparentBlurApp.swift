0b5317316f7c9db9fdb9f15a161179bb4305cf42
mk <pinch24@gmail.com>
2023-07-08 23:11:03 +0900

    Kavsoft - TransparentBlur

---
+++ Kavsoft/2023-06-22 TransparentBlur/TransparentBlur/TransparentBlurApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransparentBlurApp.swift
+//  TransparentBlur
+//
+//  Created by mk on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct TransparentBlurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
