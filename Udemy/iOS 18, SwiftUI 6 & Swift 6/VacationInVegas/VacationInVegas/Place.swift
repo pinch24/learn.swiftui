c629a7067094fb95950abb63d77f7a04279336e2
mk <pinch24@gmail.com>
2024-10-13 16:06:28 +0900

    Udemy - iOS 18, SwiftData Predicate

--- Udemy/iOS 18, SwiftUI 6 & Swift 6/VacationInVegas/VacationInVegas/Place.swift
+++ Udemy/iOS 18, SwiftUI 6 & Swift 6/VacationInVegas/VacationInVegas/Place.swift
@@ -50,7 +50,7 @@ class Place {
             Place(name: "Paris", latitude: 36.1125, longitude: -115.1707, interested: true),
             Place(name: "Treasure Island", latitude: 36.1247, longitude: -115.1721, interested: false),
             Place(name: "Stratosphere", latitude: 36.1475, longitude: -115.1566, interested: false),
-            Place(name: "Luxor", latitude: 36.0955, longitude: -115.1761, interested: true),
+            Place(name: "Luxor", latitude: 36.0955, longitude: -115.1761, interested: false),
             Place(name: "Excalibur", latitude: 36.0988, longitude: -115.1754, interested: true)
         ]
     }
--- Udemy/iOS 18, SwiftUI 6 & Swift 6/VacationInVegas/VacationInVegas/PlaceList.swift
+++ Udemy/iOS 18, SwiftUI 6 & Swift 6/VacationInVegas/VacationInVegas/PlaceList.swift
@@ -12,10 +12,29 @@ struct PlaceList: View {
     @Query(sort: \Place.name) private var places: [Place]
     
     @State private var showImages = false
+    @State private var searchText = ""
+    @State private var filterByInterested = false
+    
+    private var predicate: Predicate<Place> {
+        #Predicate<Place> {
+            if searchText.isEmpty == false && filterByInterested {
+                $0.name.localizedStandardContains(searchText) && $0.interested
+            }
+            else if searchText.isEmpty == false {
+                $0.name.localizedStandardContains(searchText)
+            }
+            else if filterByInterested {
+                $0.interested
+            }
+            else {
+                true
+            }
+        }
+    }
     
     var body: some View {
         NavigationStack {
-            List(places) { place in
+            List((try? places.filter(predicate)) ?? places) { place in
                 HStack {
                     place.image
                         .resizable()
@@ -34,11 +53,23 @@ struct PlaceList: View {
                     }
                 }
             }
+            .navigationTitle("Places")
+            .searchable(text: $searchText, prompt: "Find a Place")
+            .animation(.default, value: searchText)
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Show Images", systemImage: "photo") {
                         showImages.toggle()
                     }
+                }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Filter", systemImage: filterByInterested ? "star.fill" : "star") {
+                        withAnimation {
+                            filterByInterested.toggle()
+                        }
+                    }
+                    .tint(filterByInterested ? .yellow : .blue)
                 }
             }
             .sheet(isPresented: $showImages) {
