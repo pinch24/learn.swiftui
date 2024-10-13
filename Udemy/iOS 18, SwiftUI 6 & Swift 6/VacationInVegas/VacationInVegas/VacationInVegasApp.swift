aa0f4ace2596a9767f2913dea4ddc477b0a53ff5
mk <pinch24@gmail.com>
2024-10-13 14:05:13 +0900

    Udemy - iOS 18, ScrollView

---
+++ Udemy/iOS 18, SwiftUI 6 & Swift 6/VacationInVegas/VacationInVegas/VacationInVegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInVegasApp.swift
+//  VacationInVegas
+//
+//  Created by MK on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct VacationInVegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
