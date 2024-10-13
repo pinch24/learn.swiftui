a764895a1e0e9f111fca1822341e5244bb740e06
mk <pinch24@gmail.com>
2024-10-14 01:33:43 +0900

    Udemy - iOS 18, Swift Charts

--- Udemy/iOS 18, SwiftUI 6 & Swift 6/VacationInVegas/VacationInVegas/VegasChart.swift
+++ Udemy/iOS 18, SwiftUI 6 & Swift 6/VacationInVegas/VacationInVegas/VegasChart.swift
@@ -6,10 +6,29 @@
 //
 
 import SwiftUI
+import Charts
+
+struct SampleRating {
+    let place: String
+    let rating: Int
+    
+    static let ratings: [SampleRating] = [
+        SampleRating(place: "Bellagio", rating: 88),
+        SampleRating(place: "Paris", rating: 75),
+        SampleRating(place: "Treasure Island", rating: 33),
+        SampleRating(place: "Excalibur", rating: 99)
+    ]
+}
 
 struct VegasChart: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Chart(SampleRating.ratings, id: \.place) { rating in
+            SectorMark(angle: .value("Ratings", rating.rating), innerRadius: .ratio(0.25), angularInset: 1)
+                .cornerRadius(7)
+                .foregroundStyle(by: .value("Place", rating.place))
+        }
+        .padding()
+        .frame(height: 500)
     }
 }
 
