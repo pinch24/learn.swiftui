697d7ef274b01341289210b78bc28746f889042e
mk <pinch24@gmail.com>
2024-10-14 01:33:25 +0900

    Udemy - iOS 18, Swift Charts

---
+++ Udemy/iOS 18, SwiftUI 6 & Swift 6/VacationInVegas/VacationInVegas/VegasChart.swift
@@ -0,0 +1,18 @@
+//
+//  VegasChart.swift
+//  VacationInVegas
+//
+//  Created by MK on 10/14/24.
+//
+
+import SwiftUI
+
+struct VegasChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VegasChart()
+}
