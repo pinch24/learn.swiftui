5b2df8689ee89036cea27669f65caafe2fb85b7d
mk <pinch24@gmail.com>
2024-10-14 10:15:48 +0900

    Udemy - iOS 18, LOTR Converter: Create and Set Up New Xcode Project

---
+++ Udemy/iOS 18, SwiftUI 6 & Swift 6/LOTRConverter17/LOTRConverter17Tests/LOTRConverter17Tests.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17Tests.swift
+//  LOTRConverter17Tests
+//
+//  Created by MK on 10/14/24.
+//
+
+import Testing
+@testable import LOTRConverter17
+
+struct LOTRConverter17Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
