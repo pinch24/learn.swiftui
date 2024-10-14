9064edf1948089d393ffcb3457f46f9b2a4982bd
mk <pinch24@gmail.com>
2024-10-14 11:29:47 +0900

    Udemy - iOS 18, BB Quotes: Fetch Functions

--- Udemy/iOS 18, SwiftUI 6 & Swift 6/BBQuotes17/BBQuotes17/FetchService.swift
+++ Udemy/iOS 18, SwiftUI 6 & Swift 6/BBQuotes17/BBQuotes17/FetchService.swift
@@ -16,18 +16,47 @@ struct FetchService {
     
     // https://breaking-bad-api-six.vercel.app/api/quotes/random?production=Breaking+Bad
     func fetchQuote(from show: String) async throws -> Quote {
-        // Build Fetch URL
         let quoteURL = baseURL.appending(path: "quote/random")
         let fetchURL = quoteURL.appending(queryItems: [URLQueryItem(name: "production", value: show)])
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
         
-        // Fetch Data
-        //let response = try await URLSession.shared.data(from: quoteURL)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        let quote = try JSONDecoder().decode(Quote.self, from: data)
+        return quote
+    }
+    
+    func fetchCharacter(_ name: String) async throws -> Character {
+        let characterURL = baseURL.appending(path: "characters")
+        let fetchURL = characterURL.appending(queryItems: [URLQueryItem(name: "name", value: name)])
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
         
-        // Handle Response
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let characters = try decoder.decode([Character].self, from: data)
+        return characters[0]
+    }
+    
+    func fetchDeath(for character: String) async throws -> Death? {
+        let fetchURL = baseURL.appending(path: "deaths")
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
         
-        // Decode Data
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw FetchError.badResponse
+        }
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let deaths = try decoder.decode([Death].self, from: data)
         
-        // Reture Quote
-        return Quote(quote: "", character: "")
+        for death in deaths {
+            if death.character == character {
+                return death
+            }
+        }
+        return nil
     }
 }
