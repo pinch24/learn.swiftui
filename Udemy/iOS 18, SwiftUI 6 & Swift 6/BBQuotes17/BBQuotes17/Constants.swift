aeebadbc545fac250922b1ae604ca6dc1fcfe797
mk <pinch24@gmail.com>
2024-10-17 15:44:58 +0900

    Udemy - iOS 18, BB Quotes: Extensions & Constants

---
+++ Udemy/iOS 18, SwiftUI 6 & Swift 6/BBQuotes17/BBQuotes17/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  BBQuotes17
+//
+//  Created by MK on 10/17/24.
+//
+
+import Foundation
+
+enum Constants {
+	static let bbName = "Breaking Bad"
+	static let bcsName = "Better Call Saul"
+	static let ecName = "El Camino"
+}
