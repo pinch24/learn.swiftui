6d7905101ea91f42fa768192807cbaef6dec9a43
mk <pinch24@gmail.com>
2024-10-19 20:07:40 +0900

    Udemy - iOS 18, JP Apex

---
+++ Udemy/iOS 18, SwiftUI 6 & Swift 6/JPApex/JPApex/ApexPredator.swift
@@ -0,0 +1,69 @@
+//
+//  ApexPredator.swift
+//  JPApex
+//
+//  Created by MK on 10/19/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct ApexPredator: Decodable, Identifiable {
+	let id: Int
+	let name: String
+	let type: PredatorType
+	let latitude: Double
+	let longitude: Double
+	let movies: [String]
+	let movieScenes: [MovieScene]
+	let link: String
+	
+	var image: String {
+		name.lowercased().replacingOccurrences(of: " ", with: "")
+	}
+	
+	var location: CLLocationCoordinate2D {
+		CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+	}
+
+	struct MovieScene: Decodable, Identifiable {
+		let id: Int
+		let movie: String
+		let sceneDescription: String
+	}
+}
+
+enum PredatorType: String, Decodable, CaseIterable, Identifiable {
+	case all
+	case land
+	case air
+	case sea
+	
+	var id: PredatorType { self }
+	
+	var background: Color {
+		switch self {
+			case .all:
+					.black
+			case .land:
+					.brown
+			case .air:
+					.teal
+			case .sea:
+					.blue
+		}
+	}
+	
+	var icon: String {
+		switch self {
+			case .all:
+				"square.stack.3d.up.fill"
+			case .land:
+				"leaf.fill"
+			case .air:
+				"wind"
+			case .sea:
+				"drop.fill"
+		}
+	}
+}
---
+++ Udemy/iOS 18, SwiftUI 6 & Swift 6/JPApex/JPApexTests/JPApexTests.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexTests.swift
+//  JPApexTests
+//
+//  Created by MK on 10/19/24.
+//
+
+import Testing
+@testable import JPApex
+
+struct JPApexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
