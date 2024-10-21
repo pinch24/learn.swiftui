9e79a02326d606e1a694d99cf17ad8f58f49f535
mk <pinch24@gmail.com>
2024-10-21 10:41:18 +0900

    D&C - iOS 17, Scroll View Paging

--- DesignCode/SwiftUI for iOS 17/DesignCodeiOS17/CardView.swift
+++ DesignCode/SwiftUI for iOS 17/DesignCodeiOS17/CardView.swift
@@ -39,7 +39,7 @@ struct CardView: View {
 	var body: some View {
 		TimelineView(.animation) { context in
 			layout
-				.frame(maxWidth: 400)
+				.frame(maxWidth: 393)
 				.frame(maxWidth: .infinity, maxHeight: .infinity)
 				.padding(.vertical, 10)
 				.background(.blue.opacity(0.001))
@@ -60,8 +60,8 @@ struct CardView: View {
 				card.image
 					.resizable()
 					.aspectRatio(contentMode: .fill)
-					.frame(height: isTapped ? 600 : 300)
-					.frame(width: isTapped ? 402 : 360)
+					.frame(height: isTapped ? 600 : 500)
+					.frame(width: isTapped ? 393 : 360)
 					.if(hasPattern) { view in
 						view.colorEffect(ShaderLibrary.circleLoader(.boundingRect, .float(startDate.timeIntervalSinceNow)), isEnabled: hasPattern)
 					}
--- DesignCode/SwiftUI for iOS 17/DesignCodeiOS17/ContentView.swift
+++ DesignCode/SwiftUI for iOS 17/DesignCodeiOS17/ContentView.swift
@@ -9,16 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
 	var body: some View {
-		ScrollView {
-			VStack(alignment: .leading) {
-				Text("Explore")
-					.font(.largeTitle.weight(.bold))
-				Text("\(Date().formatted(date: .complete, time: .omitted))")
-					.foregroundStyle(.secondary)
-			}
-			.frame(maxWidth: .infinity, alignment: .leading)
-			.padding(20)
-			VStack(spacing: 60) {
+		ScrollView(.vertical, showsIndicators: false) {
+			title
+			VStack(spacing: 300) {
 				ForEach(cards) { card in
 					CardView(card: card)
 						.scrollTransition { content, phase in
@@ -26,12 +19,26 @@ struct ContentView: View {
 								.scaleEffect(phase.isIdentity ? 1 : 0.8)
 								.rotationEffect(.degrees(phase.isIdentity ? 0 : -30))
 								.rotation3DEffect(.degrees(phase.isIdentity ? 0 : 60), axis: (x: -1, y: 1, z: 0))
-								.blur(radius: phase.isIdentity ? 0 : 10)
-								.offset(x: phase.isIdentity ? 0 : -200)
+								.blur(radius: phase.isIdentity ? 0 : 60)
+								.offset(y: phase.isIdentity ? 0 : -200)
 						}
 				}
 			}
+			.scrollTargetLayout()
+			.padding(.bottom, 100)
 		}
+		.scrollTargetBehavior(.paging)
+	}
+	
+	var title: some View {
+		VStack(alignment: .leading) {
+			Text("Explore")
+				.font(.largeTitle.weight(.bold))
+			Text("\(Date().formatted(date: .complete, time: .omitted))")
+				.foregroundStyle(.secondary)
+		}
+		.frame(maxWidth: .infinity, alignment: .leading)
+		.padding(20)
 	}
 }
 
