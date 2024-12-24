775ff83194e7d3b9d9aec72714d2680e7e7ae426
mk <pinch24@gmail.com>
2024-12-24 16:32:50 +0900

    Sample - API Client

--- Sample/APIClient/APIClient/ContentView.swift
+++ Sample/APIClient/APIClient/ContentView.swift
@@ -9,25 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
 	@State private var imageData: Data?
+	@State private var showProgress: Bool = false
 	
 	var body: some View {
 		VStack {
-			if let imageData = imageData, let uiImage = UIImage(data: imageData) {
-				Image(uiImage: uiImage)
-					.resizable()
-					.scaledToFit()
-			} else {
-				Text("Loading image...")
+			ZStack {
+				if let imageData = imageData, let uiImage = UIImage(data: imageData) {
+					Image(uiImage: uiImage)
+						.resizable()
+						.scaledToFit()
+				}
+				if showProgress {
+					ProgressView()
+				}
 			}
-		}
-		.task {
-			fetchRandomImage()
+			Button("Fetch Image") {
+				fetchRandomImage()
+			}
+			.padding()
+			.background(Color.blue)
+			.foregroundColor(.white)
+			.cornerRadius(16)
+			.shadow(radius: 8)
 		}
 	}
 	
 	func fetchRandomImage() {
+		showProgress = true
 		let request = HTTPRequest<Data>(method: .GET, url: URL(string: "https://picsum.photos/400/600")!)
 		APIClient.request(request) { data in
+			showProgress = false
 			self.imageData = data
 		}
 	}
