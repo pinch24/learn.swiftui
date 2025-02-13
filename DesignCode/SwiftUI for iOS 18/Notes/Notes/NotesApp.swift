2f3f53e3aeddd5d251c4e8f2861da2b8b0e765f4
mk <pinch24@gmail.com>
2025-02-13 09:53:08 +0900

    D&C - Notes for iOS 18

---
+++ DesignCode/SwiftUI for iOS 18/Notes/Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Mk on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
