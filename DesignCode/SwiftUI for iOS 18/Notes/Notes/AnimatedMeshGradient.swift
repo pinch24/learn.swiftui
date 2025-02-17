131921c6c96d7d323cea9ad47c12f3f76d72be22
mk <pinch24@gmail.com>
2025-02-17 20:07:16 +0900

    D&C - iOS 18, Animated Mesh Gradient

--- DesignCode/SwiftUI for iOS 18/Notes/Notes/AnimatedMeshGradient.swift
+++ DesignCode/SwiftUI for iOS 18/Notes/Notes/AnimatedMeshGradient.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct AnimatedMeshGradient: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	@State var appear = false
+	@State var appear2 = false
+	
+	var body: some View {
+		MeshGradient(
+			width: 3,
+			height: 3,
+			points: [
+				[0.0, 0.0], [appear2 ? 0.5 : 1.0, 0.0], [1.0, 0.0],
+				[0.0, 0.5], appear ? [0.1, 0.5] : [0.8, 0.2], [1.0, -0.5],
+				[0.0, 1.0], [1.0, appear2 ? 2.0 : 1.0], [1.0, 1.0]
+			], colors: [
+				appear2 ? .red : .mint, appear2 ? .yellow : .cyan, .orange,
+				appear ? .blue : .red, appear ? .cyan : .white, appear ? .red : .purple,
+				appear ? .red : .cyan, appear ? .mint : .blue, appear2 ? .red : .blue
+			]
+		)
+		.onAppear {
+			withAnimation(.easeInOut(duration: 1).repeatForever(autoreverses: true)) {
+				appear.toggle()
+			}
+			withAnimation(.easeInOut(duration: 2).repeatForever(autoreverses: true)) {
+				appear2.toggle()
+			}
+		}
+	}
 }
 
 #Preview {
-    AnimatedMeshGradient()
+	AnimatedMeshGradient()
+		.ignoresSafeArea()
 }
--- DesignCode/SwiftUI for iOS 18/Notes/Notes/GenerateNoteView.swift
+++ DesignCode/SwiftUI for iOS 18/Notes/Notes/GenerateNoteView.swift
@@ -57,20 +57,12 @@ struct GenerateNoteView: View {
 					.padding()
 					.frame(maxWidth: .infinity)
 					.background(
-						MeshGradient(width: 3, height: 3, points: [
-							.init(0, 0), .init(0.5, 0), .init(1, 0),
-							.init(0, 0.5), .init(0.5, 0.5), .init(1, 0.5),
-							.init(0, 1), .init(0.5, 1), .init(1, 1)
-						], colors: [
-							.blue, .purple, .indigo,
-							.orange, .white, .blue,
-							.yellow, .green, .mint
-						])
-						.mask(
-							RoundedRectangle(cornerRadius: 16)
-								.stroke(lineWidth: 16)
-								.blur(radius: 8)
-						)
+						AnimatedMeshGradient()
+							.mask(
+								RoundedRectangle(cornerRadius: 16)
+									.stroke(lineWidth: 16)
+									.blur(radius: 8)
+							)
 					)
 					.overlay(
 						RoundedRectangle(cornerRadius: 16)
