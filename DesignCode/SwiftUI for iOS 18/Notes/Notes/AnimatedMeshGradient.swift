1941ae8e80616a7a33f5d984c8725a7418262afe
mk <pinch24@gmail.com>
2025-02-17 20:07:01 +0900

    D&C - iOS 18, Animated Mesh Gradient

---
+++ DesignCode/SwiftUI for iOS 18/Notes/Notes/AnimatedMeshGradient.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedMeshGradient.swift
+//  Notes
+//
+//  Created by Mk on 2/17/25.
+//
+
+import SwiftUI
+
+struct AnimatedMeshGradient: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedMeshGradient()
+}
