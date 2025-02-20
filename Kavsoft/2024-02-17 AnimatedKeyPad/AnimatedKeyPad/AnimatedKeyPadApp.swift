65c4f57030dab4d0c5ebb280739c61e8bee3ee2e
mk <pinch24@gmail.com>
2025-02-20 11:46:05 +0900

    Kav - AnimatedKeyPad

---
+++ Kavsoft/2024-02-17 AnimatedKeyPad/AnimatedKeyPad/AnimatedKeyPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedKeyPadApp.swift
+//  AnimatedKeyPad
+//
+//  Created by Mk on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedKeyPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
