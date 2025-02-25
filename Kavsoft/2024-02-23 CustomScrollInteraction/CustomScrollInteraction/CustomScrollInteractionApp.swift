bd0e275581b66d37698e323052adf47e5bddd342
mk <pinch24@gmail.com>
2025-02-25 16:39:01 +0900

    Kav - Custom Scroll Indicators

---
+++ Kavsoft/2024-02-23 CustomScrollInteraction/CustomScrollInteraction/CustomScrollInteractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomScrollInteractionApp.swift
+//  CustomScrollInteraction
+//
+//  Created by Mk on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomScrollInteractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
