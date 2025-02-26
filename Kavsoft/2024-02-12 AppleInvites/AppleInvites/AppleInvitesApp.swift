3fb9a91fca6117c7ccaf76df77efb3ec971423d0
mk <pinch24@gmail.com>
2025-02-26 19:12:37 +0900

    Kav - Apple Invites

---
+++ Kavsoft/2024-02-12 AppleInvites/AppleInvites/AppleInvitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleInvitesApp.swift
+//  AppleInvites
+//
+//  Created by Mk on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleInvitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
