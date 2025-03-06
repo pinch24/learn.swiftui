d985b3043e9b4c24c69e8515cdb0bb28e7bc0708
mk <pinch24@gmail.com>
2025-03-06 13:18:18 +0900

    Kav - Apple Books

---
+++ Kavsoft/2024-02-26 AppleBooks/AppleBooks/AppleBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleBooksApp.swift
+//  AppleBooks
+//
+//  Created by Mk on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kavsoft/2024-02-26 AppleBooks/AppleBooks/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  AppleBooks
+//
+//  Created by Mk on 2/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        SearchView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
